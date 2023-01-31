0a992e7714af7f2735bc32f09ba81fab6aebab14
Kaushal Singh <88377780+KaushalSingh567@users.noreply.github.com>
2023-01-31 17:44:58 +0530

    Add files via upload

---
+++ TextFormn.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+
+const TextFormn = (props) => {
+  const [textvalue, setTextvalue] = useState('');
+
+  const handleuppercase = () => {
+    const newvalue = textvalue.toUpperCase();
+    setTextvalue(newvalue);
+  }
+
+  const handlelowercase = () => {
+    const newvalue = textvalue.toLowerCase();
+    setTextvalue(newvalue);
+  }
+
+  const handleOnChange = (e) => {
+    setTextvalue(e.target.value);
+  }
+
+  return (
+    <>
+      <div>
+        <h2>{props.Heading}</h2>
+        <textarea placeholder='Enter any Text' value={textvalue} onChange={handleOnChange} />
+        <button onClick={handleuppercase}>Convert to Uppercase</button>
+        <button onClick={handlelowercase}>Convert to Lowercase</button>
+      </div>
+      <div>
+        <h2>Your Text Summary</h2>
+        <p>{textvalue.split(' ').length} words and {textvalue.length} chracters</p>
+      </div>
+    </>
+  )
+}
+
+export default TextFormn
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
